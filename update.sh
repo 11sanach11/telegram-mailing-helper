f44b572c73e8637a87eb8f3cdcc676a92707c0e3
cherkashin <alexander.cherkashin@sibit.tech>
2021-10-23 13:49:10 +0200

    0.3.14 installation: fixex in update.sh script

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "Workdir: $workdir"
 [[ -f $workdir/update_config.sh ]] && source $workdir/update_config.sh
 [[ $SERVICE_NAME ]] && service_name="$SERVICE_NAME" || service_name="telegram-mailing-helper"
 echo "Try to stop service $service_name..."
-sudo service $SERVICE_NAME stop
+sudo service $service_name stop
 echo "CURRENT VERSION: $(git -C $workdir describe --tags --abbrev=0)"
 git -C $workdir pull || true
 echo "list of available version:"
@@ -17,5 +17,5 @@ echo "Update project dependencies..."
 cd $workdir
 poetry install
 echo "Start..."
-sudo service $SERVICE_NAME start
+sudo service $service_name start
 echo "Started... please check info! on 'http://host:port/info"
